f7df77731e2aa912aa3ef123990b96500a087238
Chowdam Naga Jyothi <chowdam.n.jyothi@intimetec.com>
2021-04-14 09:23:55 +0530

    All modified

---
+++ AssetInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace AssetManagement
+{
+    public class AssetInformation
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public int Year{get;set;}
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,170 +1,204 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace AssetManagement
 {
-    public class AssetInformation
+    public class Program
     {
-        //member variables
-        int id;
-        String name;
-        int year;
-        //member functions
-        public void UserData()
+        public class AssetHelper
         {
-            //printing on output screen
-            Console.WriteLine("Enter  Asset_id:");
-            id = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Asset_name:");
-            name = (Console.ReadLine());
-
-            Console.WriteLine("Date of publishing");
-            year = int.Parse(Console.ReadLine());
-        }
-
-        //Function to display the data
-        public void Display()
-        {
-            Console.WriteLine(id + "\t\t" + name + "\t\t" + year);
-        }
-    }
-    public class AssetHelper
-    {
-        static AssetInformation[] assetInformation = new AssetInformation[100];
-        //Function for Adding Asset details
-        public static void Add_Asset()
-        {
-            Console.WriteLine("Enter Number of items to add");
-            int number = int.Parse(Console.ReadLine());
-            int index;
-            //details storing in array
-            for (index = 0; index < number; index++)
+            static List<AssetInformation> assetList = new List<AssetInformation>();
+            // To Add the information into Assets
+            public static void AddAsset()
             {
-                assetInformation[index] = new AssetInformation();
-                assetInformation[index].UserData();
+                Console.WriteLine("Enter how many records you want records:");
+                int noOfRecords = Convert.ToInt32(Console.ReadLine());
+                for (int item = 0; item < noOfRecords; item++)
+                {
+                    AssetInformation obj = new AssetInformation();
+                    Console.WriteLine("Enter Id");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter year");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    obj.Id = id;
+                    obj.Name = name;
+                    obj.Year = year;
+                    assetList.Add(obj);
+                }
+                Console.WriteLine("Id\tName\tyear\t");
+                assetList.ForEach((AssetInformation) => Console.WriteLine("{0}\t{1}\t{2}", AssetInformation.Id, AssetInformation.Name, AssetInformation.Year));
             }
-            Console.WriteLine("Asset_ID \t Asset_Name  \t year_of_publishing");
-            //To display the list
-            for (index = 0; index < number; index++)
+            //To search the given asset
+            public static void SearchAsset()
             {
-                assetInformation[index].Display();
-            }
-        }
-        //Searching method
-        public static void Search_Asset(int id)
-        {
-            Console.WriteLine("Enter the searching id");
-            int keyId = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the searching id");
+                int keyId = int.Parse(Console.ReadLine());
 
-            //if keyid matches with available id it prints successfull message
-            if (id == keyId)
-            {
-                Console.WriteLine("Given Asset_id is available");
+                assetList.ForEach((AssetInformation) =>
+                {
+                    if (AssetInformation.Id == keyId)
+                    {
+                        Console.WriteLine("{0}\t{1}\t{2}", AssetInformation.Id, AssetInformation.Name, AssetInformation.Year);
+                        Console.WriteLine("search id is available");
+                    }
+                });
             }
-            else
+            //To update the given asset
+            public static void UpdateAsset()
             {
-                Console.WriteLine("Given Asset_Id not available");
-            }
-        }
-        //Deleting information from list
-        public static void Delete_Asset(int id)
-        {
+                Console.WriteLine("which field you want to update:1.Name/t2.year");
+                int num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter which name you want to change");
+                        string keyName = (Console.ReadLine());
 
-            Console.WriteLine("Enter the searching id");
-            int keyId = int.Parse(Console.ReadLine());
-            //if given id found enters in to if condition
-            if (id == keyId)
-            {
-                //finding given id position
-                int numIdx = Array.IndexOf(assetInformation, keyId);
-                List<int> Item = new List<int> { 11, 22, 33, 44, 55 };
-                //based on index the element will deleted
-                Item.RemoveAt(numIdx);
-                Console.WriteLine("Successfully removed");
-            }
-            else
-            {
-                Console.WriteLine("No Id is there");
-            }
-        }
-        //insertion method
-        public static void Insert()
-        {
-            for (int i = 0; i < 1; i++)
-            {
-                assetInformation[i] = new AssetInformation();
-                assetInformation[i].UserData();
-            }
-        }
-        public static void Update_Asset(int Id, String Name, int date)
-        {
-            //calling method
-            Insert();
-            //to display elements
-            for (int i = 0; i < 1; i++)
-            {
-                assetInformation[i].Display();
-            }
-        }
-        //main method
-        public static void Main(String[] args)
-        {
-            Console.WriteLine("Enter Which Asset you want");
-            Console.WriteLine(" 1.Books\n 2.SoftwareLicence\n 3.Hardware\n");
-            int asset = Convert.ToInt32(Console.ReadLine());
-            //switch case to select the required asset
-            switch (asset)
-            {
-                case 1:
-                    Console.WriteLine("Welcome to Book Management");
-                    Operations();
-                    break;
+                        assetList.ForEach((AssetInformation) =>
+                    {
+                        if (AssetInformation.Name == keyName)
+                        {
+                            Console.WriteLine("Enter new name");
+                            string newName = Console.ReadLine();
+                            AssetInformation.Name = newName;
+                            Console.WriteLine("{0}\t{1}\t{2}", AssetInformation.Id, AssetInformation.Name, AssetInformation.Year);
+                        }
+                    });
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter which year you want to change");
+                        int keyYear = int.Parse(Console.ReadLine());
 
-                case 2:
-                    Console.WriteLine("Welcome to SoftwareLicence Management");
-                    Operations();
-                    break;
+                        assetList.ForEach((AssetInformation) =>
+                       {
+                           if (AssetInformation.Year == keyYear)
+                           {
+                               Console.WriteLine("Enter new year");
+                               int newYear = int.Parse(Console.ReadLine());
+                               AssetInformation.Year = newYear;
+                               Console.WriteLine("{0}\t{1}\t{2}", AssetInformation.Id, AssetInformation.Name, AssetInformation.Year);
+                           }
+                       });
+                        break;
 
-                case 3:
-                    Console.WriteLine("Welcome to Hardware Management");
-                    Operations();
-                    break;
+                    default:
+                        break;
+                }
+            }
+            //  for delete an existing asset
+            public static void DeleteAsset()
+            {
+                Console.WriteLine("which Id you want to delete");
+                int keyId = int.Parse(Console.ReadLine());
+                int availableIndex = assetList.FindIndex(item => item.Id == keyId);
+                if (availableIndex > -1)
+                {
+                    assetList.RemoveAt(availableIndex);
+                    Console.WriteLine("Deleted Successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Given id not available to delete");
+                }
+            }
+            //Main method
+            public static void Main(string[] args)
+            {
+                string choice;
+                do
+                {
+                    Console.WriteLine("Enter Which Asset you want");
+                    Console.WriteLine(" 1.Books\n 2.SoftwareLicence\n 3.Hardware\n");
+                    int asset = Convert.ToInt32(Console.ReadLine());
+                    //switch case to select the required asset
+                    switch (asset)
+                    {
+                        case 1:
+                            Console.WriteLine("Welcome to Book Management");
+                            Operations();
+                            break;
 
-                default:
-                    Console.WriteLine("Wrong Entry");
-                    break;
+                        case 2:
+                            Console.WriteLine("Welcome to SoftwareLicence Management");
+                            Operations();
+
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Welcome to Hardware Management");
+                            Operations();
+                            break;
+                        default:
+                            Console.WriteLine("Wrong Entry");
+                            break;
+                    }
+                    Console.WriteLine("Do you want to continue y/n:");
+                    choice = Console.ReadLine();
+
+                } while (choice == "y");
 
             }
-        }
-        //operations for all 3 assets
-        public static void Operations()
-        {
-            Console.WriteLine("Enter which operation u want to perform\n 1.Adding\n2.Searching\n3.Deleting\n4.Updating");
-            int num = int.Parse(Console.ReadLine());
-            switch (num)
+            public static void Operations()
             {
-                case 1:
-                    //calling add_asset function
-                    AssetHelper.Add_Asset();
-                    break;
-                case 2:
-                    //calling search_asset function
-                    AssetHelper.Search_Asset(1);
-                    break;
-                case 3:
-                    //calling.delete_asset function
-                    AssetHelper.Delete_Asset(1);
-                    break;
-                case 4:
-                    //calling Update_asset function
-                    AssetHelper.Update_Asset(1, "Name", 1999);
-                    break;
-                default:
-                    break;
+                Console.WriteLine("Enter which operation u want to perform\n 1.Adding\n2.Searching\n3.Updating\n4.Deleting");
+                int userChoice = int.Parse(Console.ReadLine());
+                switch (userChoice)
+                {
+                    case 1:
+                        //calling add_asset function
+                        AssetHelper.AddAsset();
+                        break;
+                    case 2:
+                        //calling search_asset function
+                        AssetHelper.SearchAsset();
+                        break;
+                    case 3:
+                        //calling Update_asset function
+                        AssetHelper.UpdateAsset();
+                        break;
+                    case 4:
+                        //calling.delete_asset function
+                        AssetHelper.DeleteAsset();
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
